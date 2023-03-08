f13bc691058fbe8294ad7881f4347aec7d3affee
Mpablom <100524470+Mpablom@users.noreply.github.com>
2023-03-08 00:00:58 -0300

    agregué una keyframes a la foto del banner

--- FRONTEND/src/app/Components/encabezado/encabezado.component.ts
+++ FRONTEND/src/app/Components/encabezado/encabezado.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { persona } from '../../model/persona.model';
 import { PersonaService } from 'src/app/service/persona.service';
+import { trigger,style,transition,animate,state } from '@angular/animations';
 
 @Component({
   selector: 'app-encabezado',
   templateUrl: './encabezado.component.html',
-  styleUrls: ['./encabezado.component.css']
+  styleUrls: ['./encabezado.component.css'],
 })
 export class EncabezadoComponent implements OnInit {
 
